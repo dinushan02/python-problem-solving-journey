ad1954aaa7ff75a849532d290ea360a628b5ef8c
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-20 02:11:56 +0530

    feat: add problem 002 personal information

---
+++ 01-python-basics/002-display-personal-information.py
@@ -0,0 +1,15 @@
+"""
+Problem: Display Personal Information
+
+Description:
+Write a Python program that displays your name, age, and country on separate lines.
+
+Expected Output:
+Name: Dinushan
+Age: 24
+Country: Sri Lanka
+"""
+
+print("Name: Dinushan")
+print("Age: 24")
+print("Country: Sri Lanka")
