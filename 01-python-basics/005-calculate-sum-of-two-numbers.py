695f9e35e26309da4330b09a1322f5c51403aae7
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-20 02:27:47 +0530

    feat: add problem 005 sum of two numbers

---
+++ 01-python-basics/005-calculate-sum-of-two-numbers.py
@@ -0,0 +1,16 @@
+"""
+Problem: Calculate Sum of Two Numbers
+
+Description:
+Write a Python program that stores two numbers in separate variables, 
+calculates their sum, and displays the result.
+
+Expected Output:
+Sum: 40
+"""
+
+a = 15
+b = 25
+c = a + b
+
+print(f"Sum: {c}")
