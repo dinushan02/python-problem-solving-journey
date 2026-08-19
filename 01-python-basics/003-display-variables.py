7ec69c6589ccdc8e6b7ec3cf79cbcbe10d08b63a
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-20 02:14:34 +0530

    feat: add problem 003 display variables

---
+++ 01-python-basics/003-display-variables.py
@@ -0,0 +1,20 @@
+"""
+Problem: Display Variables
+
+Description:
+Write a Python program that stores your name, age, and country in separate variables 
+and displays them on separate lines.
+
+Expected Output:
+Name: Dinushan
+Age: 24
+Country: Sri Lanka
+"""
+
+name = "Dinushan"
+age = 24
+country = "Sri Lanka"
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Country: {country}")
