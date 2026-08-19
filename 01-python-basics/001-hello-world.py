1829e5c3c19f1147427ba70583c95e04e8bda8b0
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-20 02:06:53 +0530

    feat: add problem 001 hello world

---
+++ 01-python-basics/001-hello-world.py
@@ -0,0 +1,11 @@
+"""
+Problem: Hello World
+
+Description:
+Write a Python program that displays the message "Hello, World!" on the screen.
+
+Expected Output:
+Hello, World!
+"""
+
+print("Hello, World!")
