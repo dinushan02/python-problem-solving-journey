f8a7f4a260f8fb5d32a85e65994dd9c5ddc5b951
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-20 18:37:07 +0530

    feat: add problem 007 circle area

---
+++ 01-python-basics/007-calculate-area-of-circle.py
@@ -0,0 +1,20 @@
+"""
+Problem: Calculate Area of a Circle
+
+Description:
+Write a Python program that stores the radius of a circle in a variable, 
+calculates its area, and displays the result.
+
+Formula:
+Area = π × radius²
+
+Expected Output:
+Area: 78.5
+"""
+
+radius = 5
+
+area = 3.14 * radius * radius
+# area = 3.14 * radius ** 2
+
+print(f"Area: {area}")
