cbb1992657ab4ce6f8250f65b12f5cf973b23f2e
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-21 09:26:22 +0530

    feat: add problem 014 age in days

---
+++ 01-python-basics/014-calculate-age-in-days.py
@@ -0,0 +1,22 @@
+"""
+Problem: Calculate Age in Days
+
+Description:
+Write a Python program that stores a person's age in years, converts the age into 
+an approximate number of days, and displays the result.
+
+Assumption:
+1 year = 365 days
+
+Formula:
+Days = Age × 365
+
+Expected Output:
+Approximate age in days: 8760
+"""
+
+age = 24
+
+days = age * 365
+
+print(f"Approximate age in days: {days}")
