feef514cf9692cf6d20e71ae8eb99469d467d1b5
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-21 09:31:37 +0530

    feat: add problem 015 age in months

---
+++ 01-python-basics/015-calculate-age-in-months.py
@@ -0,0 +1,22 @@
+"""
+Problem: Calculate Age in Months
+
+Description:
+Write a Python program that stores a person's age in years, converts the age into 
+an approximate number of months, and displays the result.
+
+Assumption:
+1 year = 12 months
+
+Formula:
+Months = Age × 12
+
+Expected Output:
+Approximate age in months: 288
+"""
+
+age = 24
+
+months = age * 12
+
+print(f"Approximate age in months: {months}")
