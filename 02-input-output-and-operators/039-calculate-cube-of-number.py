c52599609077ea0d7ecb9f206d22f1f16ccdd8a9
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-23 14:26:27 +0530

    feat: add problem 039 cube of a number

---
+++ 02-input-output-and-operators/039-calculate-cube-of-number.py
@@ -0,0 +1,16 @@
+"""
+Problem: Calculate Cube of a Number
+
+Description:
+Write a Python program that asks the user to enter a number, calculates its cube, and displays the result.
+
+Expected Interaction:
+Enter a number: 4
+Cube: 64
+"""
+
+num = int(input("Enter a number: "))
+
+cube = num ** 3
+
+print(f"Cube: {cube}")
