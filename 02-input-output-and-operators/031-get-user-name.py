3dc9d17996a0b93fd9ed4480e90baad42b8dfc1c
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-23 13:21:16 +0530

    feat: add problem 031 user name input

---
+++ 02-input-output-and-operators/031-get-user-name.py
@@ -0,0 +1,14 @@
+"""
+Problem: Get User Name
+
+Description:
+Write a Python program that asks the user to enter their name and displays a personalized greeting.
+
+Expected Interaction:
+Enter your name: Dinushan
+Hello, Dinushan!
+"""
+
+name = input("Enter your name: ")
+
+print(f"Hello, {name}!")
