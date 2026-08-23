804169325c35c28bd19f27a92d526bab675aa12f
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-23 13:59:33 +0530

    feat: add problem 033 sum of two user input numbers

---
+++ 02-input-output-and-operators/033-add-two-user-input-numbers.py
@@ -0,0 +1,19 @@
+"""
+Problem: Add Two User Input Numbers
+
+Description:
+Write a Python program that asks the user to enter two numbers, 
+calculates their sum, and displays the result.
+
+Expected Interaction:
+Enter first number: 15
+Enter second number: 25
+Sum: 40
+"""
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+total = num1 + num2
+
+print(f"Sum: {total}")
