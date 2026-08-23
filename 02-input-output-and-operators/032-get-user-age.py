623f533033f28d889c1623b650b4614c89bef912
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-23 13:52:50 +0530

    feat: add problem 032 user age input

---
+++ 02-input-output-and-operators/032-get-user-age.py
@@ -0,0 +1,14 @@
+"""
+Problem: Get User Age
+
+Description:
+Write a Python program that asks the user to enter their age and displays their age in a sentence.
+
+Expected Interaction:
+Enter your age: 24
+Your age is 24.
+"""
+
+age = int(input("Enter your age: "))
+
+print(f"Your age is {age}.")
