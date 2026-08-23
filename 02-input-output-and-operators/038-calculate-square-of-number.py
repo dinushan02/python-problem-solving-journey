3fb8fb3d267cbe89f6cca58c30b4d560ccfcb1f6
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-23 14:22:33 +0530

    feat: add problem 038 square of a number

---
+++ 02-input-output-and-operators/038-calculate-square-of-number.py
@@ -0,0 +1,16 @@
+"""
+Problem: Calculate Square of a Number
+
+Description:
+Write a Python program that asks the user to enter a number, calculates its square, and displays the result.
+
+Expected Interaction:
+Enter a number: 7
+Square: 49
+"""
+
+num = int(input("Enter a number: "))
+
+square = num ** 2
+
+print(f"Square: {square}")
