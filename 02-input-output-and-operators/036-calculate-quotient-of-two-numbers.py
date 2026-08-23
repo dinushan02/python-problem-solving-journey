a24dc351ce37313a4413e6da82b19076b29909de
dinushan02 <dinushandinumoorthy1111@gmail.com>
2026-08-23 14:13:30 +0530

    feat: add problem 036 quotient of two numbers

---
+++ 02-input-output-and-operators/036-calculate-quotient-of-two-numbers.py
@@ -0,0 +1,19 @@
+"""
+Problem: Calculate Quotient of Two Numbers
+
+Description:
+Write a Python program that asks the user to enter two numbers, calculates the quotient of the 
+first number divided by the second number, and displays the result.
+
+Expected Interaction:
+Enter first number: 20
+Enter second number: 4
+Quotient: 5.0
+"""
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+quotient = num1 / num2
+
+print(f"Quotient: {quotient}")
